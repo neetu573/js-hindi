e1f4a94aa078606831f4ae130dfc3178c22534a6
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-05-16 05:46:31 +0000

    convert and operator

--- basics/conversion.js
+++ basics/conversion.js
@@ -24,5 +24,43 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 
 let someNumber = 22
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//                      OPERATOR'S         
+
+let value = 2
+let negtValue = -value
+//console.log(negtValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Neetu"
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +"2");
+
+//console.log( (3 + 4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2 ,num3 
+num1= num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
