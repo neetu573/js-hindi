c66b8799b53d7f3f7d1a77fb9918648798097ee9
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-05-17 06:42:24 +0000

    comparison

---
+++ basics/03conversion.js
@@ -0,0 +1,66 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//null=>0
+//undeifend , string =>NaN
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=>false
+//"" => false
+//"hitesh" =>true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//                      OPERATOR'S         
+
+let value = 2
+let negtValue = -value
+//console.log(negtValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Neetu"
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +"2");
+
+//console.log( (3 + 4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2 ,num3 
+num1= num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
