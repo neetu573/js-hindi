298296dc6c00ad6d9d45d61f0c0defd92ab89d9e
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-05-22 05:25:12 +0000

    bascis2

---
+++ 02basics/01array.js
@@ -0,0 +1,25 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman", "nagraj"]
+
+const myArr2 = new Array (1,2,3,4)
+//console.log(myArr[1]);
+
+// array methods
+
+// myArr.push(6)
+// myArr.push(7)            //add value in last
+// myArr.pop()              //remove the last value
+
+//myArr.unshift(0)            //add in the start of the list
+//myArr.shift()               //remove from start
+
+//console.log(myArr);
+//console.log(myArr.includes(9));             
+//console.log(myArr.indexOf(19));          //neg if value is not available
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
