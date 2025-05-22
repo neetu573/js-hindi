1d596d23198ba9e0317f9ba559612c46bfad752c
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-05-22 05:33:55 +0000

    array

--- 02basics/01array.js
+++ 02basics/01array.js
@@ -19,7 +19,19 @@ const myArr2 = new Array (1,2,3,4)
 //console.log(myArr.includes(9));             
 //console.log(myArr.indexOf(19));          //neg if value is not available
 
-const newArr = myArr.join()
+//const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);    //value remane same only typeof changes
 
-console.log(myArr);
-console.log(newArr);
+//slice, splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1, 3)       //remane same give slice range seprate and wothon the array
+console.log(myn1);
+
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1, 3)    //manipulate original array splice the given range
+console.log("C ",myArr);
+console.log(myn2);
